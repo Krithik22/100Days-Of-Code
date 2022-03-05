30563b79f65a9945abbdfb9da556af24f2dcaa4f
Krithik22 <krithiksaiprasad22@gmail.com>
2022-03-05 21:55:38 +0530

    third commit

--- Day 2/life_in_weeks.py
+++ Day 2/life_in_weeks.py
@@ -1,3 +1,3 @@
-age = int(input("Enter your age:"))
+age = int(input("What's your current age:"))
 age_left = 90 - age
-print(f"you have {age_left*365} days, {age_left*52} weeks and {age_left*12} months")
+print(f"You have {age_left*365} days, {age_left*52} weeks, and {age_left*12} months left")
