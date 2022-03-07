89d86a8f5b782af3ceec545a49d558e9782edf2e
Krithik22 <krithiksaiprasad22@gmail.com>
2022-03-07 23:00:58 +0530

    2nd commit

---
+++ Day 4/banker_roulette.py
@@ -0,0 +1,5 @@
+import random
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+value = random.randint(0,len(names)-1)
+print(f"{names[value]} is going to buy the meal today!")
