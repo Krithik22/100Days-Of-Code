3bec235085b84cc8819d5dd3e83e6d2f81aaddec
Krithik22 <krithiksaiprasad22@gmail.com>
2022-03-08 22:27:53 +0530

    project commit

--- Day 4/project4.py
+++ Day 4/project4.py
@@ -40,7 +40,10 @@ elif comp_choice==2:
     print(paper)
 elif comp_choice==3:
     print(scissors)
-if (user_choice==1 and comp_choice==3) or (user_choice==3 and comp_choice==2) or (user_choice==2 and comp_choice==1):
+
+if user_choice>=0 or user_choice<0:
+    print("Inavalid choice! You Lose :(")
+elif (user_choice==1 and comp_choice==3) or (user_choice==3 and comp_choice==2) or (user_choice==2 and comp_choice==1):
     print("Hooray! You Won")
 elif user_choice==comp_choice:
     print("It's a draw")
