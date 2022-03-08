b69a620085f05dac3ab9eb9e84b74a4719916356
Krithik22 <krithiksaiprasad22@gmail.com>
2022-03-08 23:12:00 +0530

    change commit

--- Day 4/project4.py
+++ Day 4/project4.py
@@ -27,16 +27,19 @@ scissors = '''
 '''
 game_images=[rock,paper,scissors]
 user_choice=int((input("Enter (1) for Rock (2) for paper and (3) for scissors\n")))
-print(game_images[user_choice-1])
-print("Computer Chose")
-comp_choice=random.randint(1,3)
-print(game_images[comp_choice-1])
-
 if user_choice>=3 or user_choice<=0:
-    print("Inavalid choice! You Lose :(")
-elif (user_choice==1 and comp_choice==3) or (user_choice==3 and comp_choice==2) or (user_choice==2 and comp_choice==1):
-    print("Hooray! You Won")
-elif user_choice==comp_choice:
-    print("It's a draw")
+    print("Invalid choice! You Lose :(")
+    # exit();
 else:
-    print("You lose :(")
+    print(game_images[user_choice-1])
+    print("Computer Chose")
+    comp_choice=random.randint(1,3)
+    print(game_images[comp_choice-1])
+
+
+    if (user_choice==1 and comp_choice==3) or (user_choice==3 and comp_choice==2) or (user_choice==2 and comp_choice==1):
+        print("Hooray! You Won")
+    elif user_choice==comp_choice:
+        print("It's a draw")
+    else:
+        print("You lose :(")
