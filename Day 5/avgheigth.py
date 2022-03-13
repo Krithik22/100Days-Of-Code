4a687280ff7e19b51ee9d6b6fc5d76995bc7c53b
Krithik22 <krithiksaiprasad22@gmail.com>
2022-03-13 10:49:10 +0530

    commit

--- Day 5/avgheigth.py
+++ Day 5/avgheigth.py
@@ -1,9 +1,14 @@
 student_heights = input("Input a list of student heights ").split()
-count=0
-sum=0
+
 for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
-    sum+=student_heights[n]
+    # sum+=student_heights[n]
+    # count+=1
+
+count=0
+sum=0
+for height in student_heights:
+    sum+=height
     count+=1
 avg=sum/count
 print(round(avg))
