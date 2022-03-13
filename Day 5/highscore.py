1f36e03cfefe8901ed9a5ac9de7faa3d11a27e3b
Krithik22 <krithiksaiprasad22@gmail.com>
2022-03-13 11:58:50 +0530

    commit

---
+++ Day 5/highscore.py
@@ -0,0 +1,11 @@
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+max_score=10
+for x in range(0, len(student_scores)):
+    if student_scores[x]>max_score:
+        max_score=student_scores[x]
+    else:
+        max_score=max_score
+print("The highest score in the class is:",max_score)
