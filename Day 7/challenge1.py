fe102a3f1223f4c93a8dbe01743f12a2d7ed33fe
Krithik22 <krithiksaiprasad22@gmail.com>
2022-07-21 00:37:07 +0530

    commit

--- Day 7/challenge1.py
+++ Day 7/challenge1.py
@@ -11,3 +11,11 @@ for letter in chosen_word:
     print("right")
   else:
     print("wrong")
+# chosen_word=word_list[random.randint(0,len(word_list)-1)]
+#  print(word)
+# guess=input("Guess a letter:").lower()
+# for i in range(len(chosen_word)):
+#   if guess in chosen_word[i]:
+#     print("right")
+#   else:
+#     print("wrong")
